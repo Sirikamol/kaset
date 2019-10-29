2ea06e6d93990b0cba9f354d91d44b35e105d6b4
3lVv0w <krseisenh@gmail.com>
2019-10-29 15:23:37 +0700

    add dynamic list

--- lib/insert_page.dart
+++ lib/insert_page.dart
@@ -22,6 +22,7 @@ class _InsertPageState extends State<InsertPage> {
   List<String> _insertProducts = List();
   var label1;
   File _image;
+  int productCount;
 
   Future getImage() async {
     var image = await ImagePicker.pickImage(source: ImageSource.camera);
@@ -31,16 +32,56 @@ class _InsertPageState extends State<InsertPage> {
     });
   }
 
+  @override
+  void initState() {
+    super.initState();
+    productCount = 0;
+  }
+
+  List<Widget> _getListings() {
+    List listings = new List<Widget>();
+    int i = 0;
+    for (i = 0; i < productCount; i++) {
+      listings.add(TextFormField(
+          decoration: InputDecoration(
+              icon: Icon(Icons.account_balance),
+              hintText: 'กรอกสินค้า',
+              labelText: 'สินค้า'),
+          onSaved: (val) => {
+                if (val.isNotEmpty) {_insertProducts.add(val)}
+              }));
+    }
+    return listings;
+  }
+
+  void addList() {
+    setState(() {
+      productCount++;
+    });
+    print(productCount);
+  }
+
+  void removeList() {
+    setState(() {
+      productCount--;
+    });
+    print(productCount);
+  }
+
   Food newFood = Food();
 
   void _onSubmit() async {
     final FormState form = _formKey.currentState;
+    print(form);
+    setState(() {
+      _insertProducts = [];
+    });
     form.save(); //This invokes each onSaved event
 
     print('Form save called, newContact is now up to date...');
     print('Name: ${newFood.nameStore}');
-    print('Name: ${newFood.category}');
-    print('Name: ${newFood.zone}');
+    print('Category: ${newFood.category}');
+    print('Zone: ${newFood.zone}');
     print(_insertProducts);
     print(_image);
     String imgUrl = await onImageUploading(_image);
@@ -59,111 +100,103 @@ class _InsertPageState extends State<InsertPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Form(
-      key: _formKey,
-      child: ListView(
-        children: <Widget>[
-          Column(
+          key: _formKey,
+          child: ListView(
             children: <Widget>[
-              RaisedButton(
-                onPressed: getImage,
-                child: Icon(Icons.add_a_photo),
-              ),
-              Center(
-                child: _image == null
-                    ? Text('Image')
-                    : Image.file(
-                        _image,
-                        width: 250,
-                        height: 150,
+              Column(
+                children: <Widget>[
+                  RaisedButton(
+                    onPressed: getImage,
+                    child: Icon(Icons.add_a_photo),
+                  ),
+                  Center(
+                    child: _image == null
+                        ? Text('Image')
+                        : Image.file(
+                            _image,
+                            width: 250,
+                            height: 150,
+                          ),
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      label1 = Text("โซน", style: TextStyle(fontSize: 18)),
+                      DropdownButton<String>(
+                        value: dropdownValue,
+                        onChanged: (String newValue) {
+                          setState(() {
+                            dropdownValue = newValue;
+                          });
+                          newFood.zone = newValue;
+                        },
+                        items: <String>['A', 'B', 'C', 'D']
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
                       ),
+                    ],
+                  )
+                ],
               ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
+              Column(
                 children: <Widget>[
-                  label1 = Text("โซน", style: TextStyle(fontSize: 18)),
-                  DropdownButton<String>(
-                    value: dropdownValue,
-                    onChanged: (String newValue) {
-                      setState(() {
-                        dropdownValue = newValue;
-                      });
-                      newFood.zone = newValue;
-                    },
-                    items: <String>['A', 'B', 'C', 'D']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        icon: Icon(Icons.account_balance),
+                        hintText: 'ชื่อร้าน',
+                        labelText: 'กรอกชื่อร้าน'),
+                    onSaved: (val) => newFood.nameStore = val,
                   ),
                 ],
-              )
-            ],
-          ),
-          Column(
-            children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(
-                    icon: Icon(Icons.account_balance),
-                    hintText: 'ชื่อร้าน',
-                    labelText: 'กรอกชื่อร้าน'),
-                onSaved: (val) => newFood.nameStore = val,
               ),
-            ],
-          ),
-          Column(
-            children: <Widget>[
-              TextFormField(
-                  decoration: InputDecoration(
-                      icon: Icon(Icons.account_balance),
-                      hintText: 'กรอกสินค้า',
-                      labelText: 'สินค้า'),
-                  onSaved: (val) => _insertProducts.insert(0, val)),
-              TextFormField(
-                  decoration: InputDecoration(
-                      // border: InputBorder.none,
-                      icon: Icon(Icons.account_balance),
-                      hintText: 'กรอกสินค้า',
-                      labelText: 'กรอกสินค้า'),
-                  onSaved: (val) => _insertProducts.insert(1, val)),
-            ],
-          ),
-          Column(
-            children: <Widget>[
-              Row(
+              Column(
+                children: _getListings(),
+              ),
+              Column(
                 children: <Widget>[
-                  label1 = Text("ประเภท",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-                  DropdownButton<String>(
-                    value: dropdownValue2,
-                    onChanged: (String newValue) {
-                      setState(() {
-                        dropdownValue2 = newValue;
-                      });
-                      newFood.category = newValue;
-                    },
-                    items: <String>['การเกษตร', 'ของกิน', 'ของใช้', 'สัตว์']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                  ),
+                  Row(
+                    children: <Widget>[
+                      label1 = Text("ประเภท",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16)),
+                      DropdownButton<String>(
+                        value: dropdownValue2,
+                        onChanged: (String newValue) {
+                          setState(() {
+                            dropdownValue2 = newValue;
+                          });
+                          newFood.category = newValue;
+                        },
+                        items: <String>['การเกษตร', 'ของกิน', 'ของใช้', 'สัตว์']
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                      ),
+                    ],
+                  )
                 ],
-              )
+              ),
+              Container(
+                  padding: EdgeInsets.only(),
+                  child: RaisedButton(
+                    child: Text('Submit'),
+                    onPressed: _onSubmit,
+                  )),
             ],
           ),
-          Container(
-              padding: EdgeInsets.only(),
-              child: RaisedButton(
-                child: Text('Submit'),
-                onPressed: _onSubmit,
-              )),
-        ],
-      ),
-    ));
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            addList();
+          },
+        ));
   }
 }
