3b1436c8974cafaa45a8f79d67cdc5f6a931661e
Sirikamol <sirikamol40.m@gmail.com>
2019-11-04 22:09:31 +0700

    edit login

--- lib/login_two.dart
+++ lib/login_two.dart
@@ -13,31 +13,160 @@ class _LoginTwoState extends State<LoginTwo> {
       appBar: AppBar(
         title: Text('Welcome to my app'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(2.0),
-        child: Center(
-            child: ButtonBar(
+      body: SingleChildScrollView(
+        child: Column(
           children: <Widget>[
-            // FlatButton(
-            //     onPressed: () => navigateToFoodEdit(context),
-            //     color: Colors.red[300],
-            //     child: Text("Food", style: TextStyle(color: Colors.black))),
-            FlatButton(
-                onPressed: () => navigateToAnimalsEdit(context),
-                color: Colors.red[300],
-                child: Text("animals", style: TextStyle(color: Colors.black))),
-            FlatButton(
-                onPressed: () => navigateToAgricultureEdit(context),
-                color: Colors.red[300],
-                child: Text("agriculture", style: TextStyle(color: Colors.black))),
-            FlatButton(
-                onPressed: () => navigateToGeneralEdit(context),
-                color: Colors.red[300],
-                child: Text("general", style: TextStyle(color: Colors.black))),
+            
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    GestureDetector(
+                      onTap: () {
+                        navigateToFoodEdit(context);
+                      },
+                      child: Card(
+                        child: Column(
+                          children: <Widget>[
+                            Padding(
+                                padding: EdgeInsets.all(7.0),
+                                child: Row(
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: EdgeInsets.all(7.0),
+                                      child: Icon(
+                                        Icons.restaurant,
+                                        color: Colors.black,
+                                        size: 50,
+                                      ),
+                                    ),
+                                  ],
+                                ))
+                          ],
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        navigateToAnimalsEdit(context);
+                      },
+                      child: Card(
+                        child: Column(
+                          children: <Widget>[
+                            Padding(
+                                padding: EdgeInsets.all(7.0),
+                                child: Row(
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: EdgeInsets.all(7.0),
+                                      child: Icon(
+                                        Icons.local_florist,
+                                        color: Colors.black,
+                                        size: 50,
+                                      ),
+                                    ),
+                                  ],
+                                ))
+                          ],
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        navigateToAgricultureEdit(context);
+                      },
+                      child: Card(
+                        child: Column(
+                          children: <Widget>[
+                            Padding(
+                                padding: EdgeInsets.all(7.0),
+                                child: Row(
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: EdgeInsets.all(7.0),
+                                      child: Icon(
+                                        Icons.pets,
+                                        color: Colors.black,
+                                        size: 50,
+                                      ),
+                                    ),
+                                  ],
+                                ))
+                          ],
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        navigateToGeneralEdit(context);
+                      },
+                      child: Card(
+                        child: Column(
+                          children: <Widget>[
+                            Padding(
+                                padding: EdgeInsets.all(7.0),
+                                child: Column(
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: EdgeInsets.all(7.0),
+                                      child: Icon(
+                                        Icons.shopping_basket,
+                                        color: Colors.black,
+                                        size: 50,
+                                      ),
+                                    ),
+                                  ],
+                                ))
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(margin: EdgeInsets.only(left: 0))
+              ],
+            ),
+            
           ],
+        ),
+      )
+
+
+
+
+      // body: Padding(
+      //   padding: const EdgeInsets.all(2.0),
+      //   // child: Center(
+      //       child: ButtonBar(
+      //     children: <Widget>[
+            
+      //       Column(
+      //         children: <Widget>[
+      //           FlatButton(
+      //               onPressed: () => navigateToFoodEdit(context),
+      //               color: Colors.red[300],
+      //             child: Text("Food", style: TextStyle(color: Colors.black))),
+      //           FlatButton(
+      //               onPressed: () => navigateToAnimalsEdit(context),
+      //               color: Colors.red[300],
+      //               child: Text("animals", style: TextStyle(color: Colors.black))),
+      //           FlatButton(
+      //               onPressed: () => navigateToAgricultureEdit(context),
+      //               color: Colors.red[300],
+      //               child: Text("agriculture", style: TextStyle(color: Colors.black))),
+      //           FlatButton(
+      //               onPressed: () => navigateToGeneralEdit(context),
+      //               color: Colors.red[300],
+      //               child: Text("general", style: TextStyle(color: Colors.black))),
+      //         ],
+      //       ),
+            
+            
+      //     ],
           
-        )),
-      ),
+      //   ),
+      // ),
     );
   }
 }
